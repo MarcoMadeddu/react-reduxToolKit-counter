c0a22e10fddaa77458adc8dd855cf2c2ad7954f5
MarcoMadeddu <marcomade99@gmail.com>
2023-02-16 16:13:55 +0100

    Change style

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import { amountAdded, incremented } from './features/Counter/counter'
 import { useAppDispatch, useAppSelector } from './app/hooks'
 import './App.css'
+import Counter from './components/Counter'
 
 function App() {
   const count = useAppSelector((state) => state.counter.value)
@@ -17,33 +18,38 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <h4>Increment by 1</h4>
-        <h3>{count}</h3>
-        <button onClick={handleClick}>
-          increment
-        </button>
-        <hr />
-        <h4>Increment by 3</h4>
-        <h3>{count}</h3>
-        <button onClick={secondHandleClick}>
-          increment
-        </button>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="app">
+      <div className="title"><h1>Redux Tutorial</h1></div>
+      <Counter></Counter>
     </div>
+
+    // <div className="App">
+    //   <div>
+    //     <a href="https://vitejs.dev" target="_blank">
+    //       <img src="/vite.svg" className="logo" alt="Vite logo" />
+    //     </a>
+    //     <a href="https://reactjs.org" target="_blank">
+    //       <img src={reactLogo} className="logo react" alt="React logo" />
+    //     </a>
+    //   </div>
+    //   <h1>Vite + React</h1>
+    //   <div className="card">
+    //     <h4>Increment by 1</h4>
+    //     <h3>{count}</h3>
+    //     <button onClick={handleClick}>
+    //       increment
+    //     </button>
+    //     <hr />
+    //     <h4>Increment by 3</h4>
+    //     <h3>{count}</h3>
+    //     <button onClick={secondHandleClick}>
+    //       increment
+    //     </button>
+    //   </div>
+    //   <p className="read-the-docs">
+    //     Click on the Vite and React logos to learn more
+    //   </p>
+    // </div>
   )
 }
 
---
+++ app/src/components/Counter/index.tsx
@@ -0,0 +1,43 @@
+import { amountAdded, incremented } from '../../features/Counter/counter'
+import { useAppDispatch, useAppSelector } from '../../app/hooks'
+
+
+// export const Counter = () => {
+//     return (
+//         <div className="counter">
+
+//         </div>
+//     )
+// }
+
+function Counter() {
+    const count = useAppSelector((state) => state.counter.value)
+    const dispatch = useAppDispatch();
+
+    const handleClick = () => {
+        dispatch(incremented())
+    }
+
+    const secondHandleClick = () => {
+        dispatch(amountAdded(3))
+    }
+
+    return (
+        <div className="counter">
+            <div className="counter-value">
+                <h2>Count is current: {count}</h2>
+            </div>
+            <div className="button-container">
+                <button onClick={handleClick}>
+                    increment
+                </button>
+                <button onClick={secondHandleClick}>
+                    increment by 3
+                </button>
+            </div>
+
+        </div>
+    )
+}
+
+export default Counter
